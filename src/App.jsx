dd676548ef24bc963768254171bef71b3ef84fff
Keshav Anand <keshavanand2001@gmail.com>
2024-02-10 17:52:46 +0530

    Todos now stay on refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 // A component is a funtion with a capital first letter that encapsulates a reusable UI element of the page.
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { NewTodoForm } from "./NewTodoForm"
 import { Todolist } from "./Todolist"
 
 export default function App(){ // you can only return 1 element in a Component
-  const [todos, setTodos]= useState([])
+  
+  const [todos, setTodos]= useState(()=>{
+    const localValue= localStorage.getItem("ITEMS")
+    if(localValue==null) return []
+    return JSON.parse(localValue)
+  })
+  
+  // dont run hooks conditionally or they will make the page not render if the condition is not satisfied
+  
+  useEffect(()=>{
+    localStorage.setItem("ITEMS", JSON.stringify(todos)) //stores info to localstorage
+  },[todos])// run this funtion every time the second property changes
 
   function addTodo(title){
     setTodos(currentTodos=>{
@@ -32,6 +43,7 @@ export default function App(){ // you can only return 1 element in a Component
       return currentTodos.filter(todo=> todo.id !== id)
     })
   }
+  
   return (
   <>
   <NewTodoForm  onSubmit={addTodo}  />
